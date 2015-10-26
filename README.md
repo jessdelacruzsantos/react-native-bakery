cd88bb037de3cf1afc415272720a5c5e20798770
Stephen Barlow <barlow@squareup.com>
2015-10-26 16:25:46 -0700

    Update PHP proto classes for transactions, add Python

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Connect SDKs
 
 This repo contains the source for the SDKs we generate for developers to use
-Connect API v2. The first two SDKs we will release are in PHP and Ruby.
+Connect API v2.
 
-## SDK Layout
-Each SDK consists of core code that is written expressly for the language,
-along with classes generated from the proto files that define the API (in the
-  `connect-public-protos` repo).
+Each language we support will have its own directory under `src`. Each language's
+directory has a `generated` directory beneath it, which contains `protoc`-generated
+wrappers for the SDK.
+
+The `script` directory contains scripts that assist with the generation of these
+wrappers, along with other tasks.
+
+**Note: The generate_proto_classes.rb script will not work for you out of the box
+yet. Soon though!**
--- script/generate_proto_classes.rb
+++ script/generate_proto_classes.rb
@@ -5,19 +5,26 @@ HOME_DIR = ENV['HOME']
 
 def generate_class(path)
 
-  # PHP
   dir_path = File.dirname(path)
   proto_path_prefix = HOME_DIR + '/Development/connect-public-protos/squareup/connect/v3/'
   proto_output_location = HOME_DIR + '/Development/connect-sdks/src/php/generated/' + dir_path.partition(proto_path_prefix).last
-  puts 'LOCATION ' + proto_output_location
 
   if !File.directory?(proto_output_location)
     FileUtils.mkdir_p(proto_output_location)
   end
 
+  # PHP
   system "protoc --plugin=protoc-gen-php='#{HOME_DIR}/Development/connect-sdks/lib/Protobuf-PHP/protoc-gen-php.php' \
           --proto_path=#{HOME_DIR}/Development/connect-public-protos \
           --php_out='skip-imported=true:#{proto_output_location}' \
+          #{path}"
+
+  # Python
+  proto_output_location = HOME_DIR + '/Development/connect-sdks/src/python/generated'
+
+  system "protoc \
+          --proto_path=#{HOME_DIR}/Development/connect-public-protos \
+          --python_out='#{proto_output_location}' \
           #{path}"
 end
 
