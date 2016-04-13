6c10f2cd1bd53abcac08d7edfae5c3e0dbfd5f80
Alec Holmes <alec@squareup.com>
2016-04-12 18:48:05 -0700

    Clean up DocString; include all types in Swagger spec

--- tools/api-parser/src/main/java/com/squareup/apiparser/DocString.java
+++ tools/api-parser/src/main/java/com/squareup/apiparser/DocString.java
@@ -1,57 +1,46 @@
 package com.squareup.apiparser;
 
+import com.google.common.base.CharMatcher;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
 import java.util.List;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 
 public class DocString {
-  private final String docString;
-  private boolean hasParsed;
-  private ImmutableMap<String, String> annotations;
+  private static final Splitter BODY_SPLITTER = Splitter.on("--").omitEmptyStrings().trimResults();
+  private static final Splitter AT_SPLITTER = Splitter.on("@").omitEmptyStrings().trimResults();
+  private static final Splitter SPACE_SPLITTER =
+      Splitter.on(CharMatcher.WHITESPACE).limit(2).trimResults();
+
+  private final ImmutableMap<String, String> annotations;
 
   public DocString(String docString) {
-    this.docString = checkNotNull(docString);
-    ;
-    this.annotations = ImmutableMap.of();
+    checkNotNull(docString);
+
+    String parseableDocString = Iterables.getLast(
+        Iterables.limit(BODY_SPLITTER.split(docString), 2), "");
+    ImmutableMap.Builder<String, String> annotationsBuilder =
+        ImmutableMap.<String, String>builder();
+
+    int firstIndex = parseableDocString.indexOf("@");
+    if (firstIndex >= 0) {
+      AT_SPLITTER.splitToList(parseableDocString.substring(firstIndex)).stream().forEach(s -> {
+        List<String> keyValue = SPACE_SPLITTER.splitToList(s);
+        checkState(keyValue.size() > 0 && keyValue.size() <= 2, "%s", keyValue);
+        String value = keyValue.size() == 2
+            ? keyValue.get(1)
+            : "";
+        annotationsBuilder.put(keyValue.get(0), value);
+      });
+    }
+
+    annotations = annotationsBuilder.build();
   }
 
   public ImmutableMap<String, String> getAnnotations() {
-    if (!hasParsed) { parse(); }
-
     return annotations;
-  }
-
-  public DocString parse() {
-    if (!hasAnnotations() || hasParsed) { return this; }
-
-    String parseableDocString = (isMultiline()) ? getMultiline() : getSingleline();
-    final List<String> splits = Splitter.on("@").trimResults().splitToList(parseableDocString);
-    ImmutableMap.Builder<String, String> b = ImmutableMap.<String, String>builder();
-    splits.forEach(s -> {
-      String k = s.split(" ")[0];
-      b.put(k, s.replaceFirst(k, "").trim());
-    });
-    this.annotations = b.build();
-    this.hasParsed = true;
-    return this;
-  }
-
-  private boolean isMultiline() {
-    // Public doc strings can be bounded by two hyphens to support multiline annotations.
-    return docString.contains("--");
-  }
-
-  private boolean hasAnnotations() {
-    return docString.contains("@");
-  }
-
-  private String getMultiline() {
-    return docString.split("--")[1];
-  }
-
-  private String getSingleline() {
-    return docString;
   }
 }
