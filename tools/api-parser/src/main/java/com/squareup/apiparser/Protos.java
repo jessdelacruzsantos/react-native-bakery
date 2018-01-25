cb1a535e5c293698ca19768559f387785e322bdd
Alec Holmes <alec@squareup.com>
2018-01-24 18:37:37 -0800

    Support fully-qualified type names

--- tools/api-parser/src/main/java/com/squareup/apiparser/Protos.java
+++ tools/api-parser/src/main/java/com/squareup/apiparser/Protos.java
@@ -3,16 +3,25 @@ package com.squareup.apiparser;
 import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Splitter;
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
-
-import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 class Protos {
+
+  private static final Set<String> PACKAGE_SEPARATORS = ImmutableSet.of(
+      "actions", "catalog_v1", "common", "resources", "resources_internal", "service");
+
   static String cleanName(String name) {
     Preconditions.checkNotNull(name);
-    final Iterable<String> strings = new LinkedList<>(Splitter.on('.').splitToList(name));
-    Iterables.removeAll(strings, ImmutableList.of("actions", "catalog_v1", "common", "resources", "resources_internal"));
+    List<String> strings = Splitter.on('.').splitToList(name);
+
+    int splitIndex = Iterables.indexOf(strings, PACKAGE_SEPARATORS::contains);
+    if (splitIndex >= 0) {
+      strings = strings.subList(splitIndex + 1, strings.size());
+    }
+
     return Joiner.on("").join(strings);
   }
 
--- tools/api-parser/src/test/java/com/squareup/apiparser/ProtosTest.java
+++ tools/api-parser/src/test/java/com/squareup/apiparser/ProtosTest.java
@@ -10,16 +10,15 @@ public class ProtosTest {
     assertThat(Protos.cleanName("a.b.c.d"), equalTo("abcd"));
 
     // Top-level groupings
-    assertThat(Protos.cleanName("squareup.connect.v2.actions.Thing"), equalTo("squareupconnectv2Thing"));
-    assertThat(Protos.cleanName("squareup.connect.v2.actions.Thing.Is.Nested"), equalTo("squareupconnectv2ThingIsNested"));
-    assertThat(Protos.cleanName("squareup.connect.v2.common.Thing"), equalTo("squareupconnectv2Thing"));
-    assertThat(Protos.cleanName("squareup.connect.v2.resources.Thing"), equalTo("squareupconnectv2Thing"));
-    assertThat(Protos.cleanName("squareup.connect.v2.resources_internal.Thing"), equalTo("squareupconnectv2Thing"));
-    assertThat(Protos.cleanName("squareup.connect.v2.service.Thing"), equalTo("squareupconnectv2serviceThing"));
+    assertThat(Protos.cleanName("squareup.connect.v2.actions.Thing"), equalTo("Thing"));
+    assertThat(Protos.cleanName("squareup.connect.v2.actions.Thing.Is.Nested"), equalTo("ThingIsNested"));
+    assertThat(Protos.cleanName("squareup.connect.v2.common.Thing"), equalTo("Thing"));
+    assertThat(Protos.cleanName("squareup.connect.v2.resources.Thing"), equalTo("Thing"));
+    assertThat(Protos.cleanName("squareup.connect.v2.resources_internal.Thing"), equalTo("Thing"));
+    assertThat(Protos.cleanName("squareup.connect.v2.service.Thing"), equalTo("Thing"));
 
     // Product groupings
-    assertThat(Protos.cleanName("squareup.connect.v2.someproduct.service.Thing"), equalTo("squareupconnectv2someproductserviceThing"));
-    assertThat(Protos.cleanName("squareup.connect.v2.someproduct.service.Thing.Is.Nested"), equalTo("squareupconnectv2someproductserviceThingIsNested"));
-    assertThat(Protos.cleanName("squareup.connect.v2.someproduct.service.Thing.action.Is.Nested"), equalTo("squareupconnectv2someproductserviceThingactionIsNested"));
+    assertThat(Protos.cleanName("squareup.connect.v2.someproduct.service.Thing"), equalTo("Thing"));
+    assertThat(Protos.cleanName("squareup.connect.v2.someproduct.service.Thing.Is.Nested"), equalTo("ThingIsNested"));
   }
 }
